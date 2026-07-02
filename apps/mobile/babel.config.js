90074978f013eb88fd22c999a40b9c389af8b4a2
Gabriele Pau <gabrielepau27@gmail.com>
2026-07-02 11:25:21 +0200

    fix: set EXPO_ROUTER_APP_ROOT via process.env in babel.config.js

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -1,14 +1,11 @@
+const path = require("path");
+
+// Set EXPO_ROUTER_APP_ROOT before the babel plugin runs
+process.env.EXPO_ROUTER_APP_ROOT = path.resolve(__dirname, "app");
+
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: [
-      [
-        "expo-router/babel",
-        {
-          root: "../../apps/mobile/app",
-        },
-      ],
-    ],
   };
 };
